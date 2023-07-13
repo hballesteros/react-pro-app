9e3e8a04e0b399b8726a34e25a83006c53784bf5
Hugo <hugo@boahstudio.com>
2023-07-13 00:34:28 -0300

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Product, onChangesArgs } from '../interfaces/interfaces';
 
 
@@ -12,17 +12,9 @@ export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => 
   
     const [counter, setCounter] = useState( value );
 
-    const isControlled = useRef( !!onChange );
-
-
     const increaseBy = (value: number) => {
-
-        if ( isControlled.current ) {
-            return onChange!({ counter: value, product });
-        }   
         
         const newValue = Math.max(0 , counter + value);
-        
         setCounter( newValue );
         
         onChange && onChange({ counter: newValue, product });
@@ -31,8 +23,7 @@ export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => 
     useEffect(() => {
         setCounter( value );
     }, [ value ])
-    
-  
+      
     return {
         counter,
         increaseBy
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,30 +9,15 @@ export const useShoppingCart = () => {
 
         setShoppingCart( prev => {
 
-            const productInCart: ProductInCart = prev[product.id] || { ...product, count: 0 };  
-
-            if ( Math.max( productInCart.count + counter, 0 ) > 0 ) {
-                productInCart.count += counter;
-                return {
-                    ...prev,
-                    [product.id]: productInCart,
-                }
+            if ( counter === 0 ) {
+                const { [product.id]: toDeleted, ...rest } = prev;
+                return rest;
             }
-            
-            // Borrar el producto del carrito si el contador es 0
-            const { [product.id]: toDeleted, ...rest } = prev;
-            return rest;
-           
 
-            // if ( counter === 0 ) {
-            //     const { [product.id]: toDeleted, ...rest } = prev;
-            //     return rest;
-            // }
-
-            // return {
-            //     ...prev,
-            //     [product.id]: { ...product, count: counter }
-            // }
+            return {
+                ...prev,
+                [product.id]: { ...product, count: counter }
+            }
         
         });
     };
