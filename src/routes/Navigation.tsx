247cf7b62966c42ed9354fed6e7896cec5784848
Hugo <hugo@boahstudio.com>
2023-07-17 00:23:35 -0300

    Initial commit forms

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -3,9 +3,6 @@ import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../assets/react.svg';
 
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
-
-
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -14,23 +11,23 @@ export const Navigation = () => {
                 <img className="logo-img" src={ logo } alt="React Logo" />
                 <ul>
                     <li>
-                        <NavLink to="/lazy1" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Lazy 1</NavLink>
+                        <NavLink to="/" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/lazy2" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Lazy 2</NavLink>
+                        <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >About</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/lazy3" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Lazy 3</NavLink>
+                        <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Users</NavLink>
                     </li>
                 </ul>
             </nav>
 
             <Routes>
-                <Route path="lazy1" element={ <LazyPage1 /> } />
-                <Route path="lazy2" element={ <LazyPage2 /> } />
-                <Route path="lazy3" element={ <LazyPage3 /> } />
+                <Route path="/" element={ <h1>Home</h1> } />
+                <Route path="about" element={ <h1>About</h1> } />
+                <Route path="users" element={ <h1>Users</h1> } />
                 
-                <Route path="/*" element={ <Navigate to="/lazy1" replace /> } />
+                <Route path="/*" element={ <Navigate to="/" replace /> } />
             </Routes>
 
         </div>
