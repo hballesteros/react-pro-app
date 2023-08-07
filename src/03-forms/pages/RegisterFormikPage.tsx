7c9b849f63842be44a1ba46dc378ed94f7e83517
Hugo <hugo@boahstudio.com>
2023-08-07 06:07:06 -0300

    Register Formik Page

---
+++ src/03-forms/pages/RegisterFormikPage.tsx
@@ -0,0 +1,79 @@
+import { FormEvent } from 'react';
+import { useForm } from '../hooks/useForms';
+
+import '../styles/styles.css';
+
+export const RegisterFormikPage = () => {
+
+    const { 
+            formData, onChange, resetForm, isValidEmail,
+            name, email, password, confirmPassword    
+        } = useForm({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+    
+    const onSubmit = (event:FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+   
+
+    return (
+        <div>
+            <h1>Register Formik Page</h1>
+
+            <form noValidate onSubmit={ onSubmit }>
+
+                <input 
+                    type="text"
+                    placeholder="Name"
+                    name = "name"
+                    value={ name }
+                    onChange={ onChange }
+                    className={ `${ name.trim().length <= 0 ? "has-error" : "" }` }
+                />
+                { name.trim().length <= 0 && <span>Este campo es necesario</span> }
+
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={ email }
+                    onChange={ onChange }
+                    className= { `${ !isValidEmail( email ) ? "has-error" : "" }` }
+                />
+                { !isValidEmail( email ) && <span>Email no es válido</span> }
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={ password }
+                    onChange={ onChange }
+                />
+                { password.trim().length <= 0 && <span>Este campo es necesario</span> }
+                { password.trim().length < 6 && password.trim().length > 0 && <span>la contraseña tiene que tener 6 letras</span> }
+
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm password"
+                    value={ confirmPassword }
+                    onChange={ onChange }
+                />
+                { confirmPassword.trim().length <= 0 && <span>Este campo es necesario</span> }
+                { confirmPassword.trim().length > 0 && password !== confirmPassword && <span>Las contraseñas no coinciden</span> }
+
+                <button type="submit">Create</button>
+                <button 
+                    type="button"
+                    onClick={ resetForm }
+                >Reset Form</button>  
+
+            </form>
+        </div>
+  )
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
-import { RegisterPage, FormikBasicPage, FormikYupPage, FormikComponents, FormikAbstraction } from '../03-forms/pages';
+import { RegisterPage, FormikBasicPage, FormikYupPage, FormikComponents, FormikAbstraction, RegisterFormikPage } from '../03-forms/pages';
 
 import logo from '../assets/react.svg';
 
@@ -28,6 +28,9 @@ export const Navigation = () => {
                         <NavLink to="/formik-abstraction" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Formik Abstraction</NavLink>
                     </li>
                     <li>
+                        <NavLink to="/formik-register" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Register Formik</NavLink>
+                    </li>
+                    <li>
                         <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' } >Users</NavLink>
                     </li>
                 </ul>
@@ -39,6 +42,7 @@ export const Navigation = () => {
                 <Route path="/formik-yup" element={ <FormikYupPage /> } />
                 <Route path="/formik-components" element={ <FormikComponents /> } />
                 <Route path="/formik-abstraction" element={ <FormikAbstraction /> } />
+                <Route path="/formik-register" element={ <RegisterFormikPage /> } />
                 <Route path="/users" element={ <h1>Users</h1> } />
                 
                 <Route path="/*" element={ <Navigate to="/" replace /> } />
